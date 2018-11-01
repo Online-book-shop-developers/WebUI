371fbeccc7067193885534876ac91d4ceea61941
HenokTes72 <henoktes72@gmail.com>
2018-11-01 13:01:10 +0300

    Chaning some style

--- src/components/HomeInfo/index.js
+++ src/components/HomeInfo/index.js
@@ -4,7 +4,7 @@ import './HomeInfo.css';
 
 const HomeInfo = () => (
     <div className="home">
-        <div style={{ flexGrow: '0.5', display: 'flex', paddingLeft: '20px', flexDirection: 'column', justifyContent: 'center', alignItems: 'flex-start' }} >
+        <div style={{ flexGrow: '0.5', display: 'flex', paddingLeft: '100px', flexDirection: 'column', justifyContent: 'center', alignItems: 'flex-start' }} >
             <strong style={{ color: 'white' ,fontSize: '63px', fontWeight: '700', fontFamily: "Fredericka the Great, cursive", textShadow: "2px 2px 4px black" }}>PEEK  A  BOOK!</strong>
             <p style={{ color: 'white' ,fontSize: '30px', textShadow: '1px 1px 2px black', letterSpacing: '3px', fontFamily: 'Amatic SC, cursive' }}>
                 SAVE 30% ON 4+ USED BOOKS
